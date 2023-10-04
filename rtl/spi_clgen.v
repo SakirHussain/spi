f891b59f9bba3db9c3cf34db3d18419da92f293b
sakirhussain <sakirhussainpa@gmail.com>
2023-10-04 15:28:07 +0530

    re wrote clgen but sclk staying active for an additional cycle still there

--- rtl/spi_clgen.v
+++ rtl/spi_clgen.v
@@ -9,49 +9,76 @@ module spi_clgen(
                     cpol_0,
                     cpol_1 );
 
-input wb_clk, wb_reset, tip, go, lstclk;
-input [31:0] divider;
+    input wb_clk, wb_reset, tip, go, lstclk;
+    input [31:0] divider;
 
-output reg sclk, cpol_0, cpol_1;
+    output reg sclk, cpol_0, cpol_1;
 
-reg [31:0] count ;
+    reg [31:0] count ;
 
-// always @(wb_reset:sensitivity_list) begin
-//     if (wb_reset == 1'b1) begin
-//         sclk <= 0;
-//         cpol_0 <= 0;
-//         cpol_1 <= 0;
-//     end
-// end
+    // always @(wb_reset:sensitivity_list) begin
+    //     if (wb_reset == 1'b1) begin
+    //         sclk <= 0;
+    //         cpol_0 <= 0;
+    //         cpol_1 <= 0;
+    //     end
+    // end
 
 
-always @(posedge wb_clk or posedge wb_reset) begin
-
-    if (wb_reset == 1'b1) begin
-        sclk <= 0;
-        cpol_0 <= 0;
-        cpol_1 <= 0;
-        count <= 32'h0000;
-    end
-    else begin
-            count <= count + 32'h0001;
-        if (count == (divider + 32'h0001)) begin
-            sclk <= ~sclk;
+    always @(posedge wb_clk or posedge wb_reset) begin
+        if (wb_reset == 1'b1) begin
             count <= 32'h0000;
         end
-
-        if (count == divider && sclk == 1'b0) begin
-            cpol_0 <= 1'b1;          //rising edge
-            #10;                    //this might need fixing
-            cpol_0 <= 1'b0;
+        else if (tip) begin
+            if (count == divider + 32'h0001) begin
+                count <= 32'h0000;
+            end
+            else begin
+                count <= count + 1;
+            end
+        // else if (count) begin
+        //     count <= 32'h0004
+        // end        
         end
-        else if (count == divider && sclk == 1'b1) begin
-            cpol_1 <= 1'b1;                  //falling edge
-            #10;                    //this might need fixing
+    end
+
+    always @(posedge wb_clk or posedge wb_reset) begin
+        if (wb_reset) begin
+            sclk <= 1'b0;
+        end
+        else if (tip) begin
+            if(count == (divider + 32'h0001)) begin
+                if(!lstclk || sclk) begin
+                    sclk = ~sclk;
+                end
+            end
+        end
+    end
+
+    always @(posedge wb_clk or posedge wb_reset) begin
+        if (wb_reset) begin
+            cpol_0 <= 1'b0;
             cpol_1 <= 1'b0;
         end
-    end    
-end
+        else begin
+            cpol_0 <= 1'b0;
+            cpol_1 <= 1'b0;
+            if (tip) begin
+                if (~sclk) begin
+                    if (count == divider) begin
+                        cpol_0 <= 1'b1;
+                    end
+                end
+            end
 
+            if (tip) begin
+                if (sclk) begin
+                    if(count == divider) begin
+                        cpol_1 <= 1'b1;
+                    end
+                end
+            end
+        end
+    end
 endmodule
 
--- tb/spi_clgen_tb.v
+++ tb/spi_clgen_tb.v
@@ -1,69 +1,62 @@
-module spi_clgen_tb();
+module spi_clgen_tb;
 
-  // Define constants for clock period and reset duration
-  reg clk;
-  reg wb_reset;
-  reg tip;
-  reg go;
-  reg lstclk;
-  reg [31:0] divider;
-  
-  wire sclk;
-  wire cpol_0;
-  wire cpol_1;
+    // Inputs
+    reg wb_clk;
+    reg wb_reset;
+    reg tip;
+    reg go;
+    reg lstclk;
+    reg [31:0] divider;
 
-  // Instantiate the spi_clgen module
-  spi_clgen uut (
-    .wb_clk(clk),
-    .wb_reset(wb_reset),
-    .tip(tip),
-    .go(go),
-    .lstclk(lstclk),
-    .divider(divider),
-    .sclk(sclk),
-    .cpol_0(cpol_0),
-    .cpol_1(cpol_1)
-  );
+    // Outputs
+    wire sclk;
+    wire cpol_0;
+    wire cpol_1;
 
-  // Clock generation
-  initial begin
-    clk = 1'b0;
-    forever begin
-        #5 clk = ~clk;
+    // Instantiate the spi_clgen module
+    spi_clgen uut (
+        .wb_clk(wb_clk),
+        .wb_reset(wb_reset),
+        .tip(tip),
+        .go(go),
+        .lstclk(lstclk),
+        .divider(divider),
+        .sclk(sclk),
+        .cpol_0(cpol_0),
+        .cpol_1(cpol_1)
+    );
+
+    // Clock generation
+    initial begin
+      wb_clk = 1'b0;
+      forever begin
+        #5 wb_clk = ~wb_clk;
+      end
     end
-  end
-  
-  // Initialize inputs
-  initial begin
-    // Initialize inputs
-    wb_reset = 1'b0;
-    tip = 1'b0;
-    go = 1'b0;
-    lstclk = 1'b0;
-    divider = 32'h0004; // Set your desired divider value here
 
-    // Apply reset
-    wb_reset = 1'b1;
-    #10; // Hold reset for 20 time units
-    wb_reset = 1'b0;
+    // Initializations
+    initial begin
+        // wb_clk = 0;
+        wb_reset = 0;
+        tip = 1;
+        go = 0;
+        lstclk = 0;
+        divider = 32'h0004; // You can set the divider value as needed
+        
+        // Apply reset
+        wb_reset = 1;
+        #10 wb_reset = 0;
 
-    // Generate stimulus
-    tip = 1'b1;
-    go = 1'b1;
-    lstclk = 1'b0;
-    #100; // Run the simulation for a while
-    tip = 1'b0;
-    go = 1'b0;
-    lstclk = 1'b0;
-    #100; // Run the simulation for a while
+        // Apply tip (toggle it)
+        // tip = 1;
+        // #10 tip = 0;
 
-    // Finish simulation
-    $finish;
-  end
+        // Simulate for a period of time
+        #1000 $finish; // Finish the simulation after 1000 time units
+    end
 
-  // Monitor outputs
-  initial
-    $display("sclk = %b, cpol_0 = %b, cpol_1 = %b", sclk, cpol_0, cpol_1);
-  
+    // Monitor and display outputs
+    initial
+        $display("sclk = %b, cpol_0 = %b, cpol_1 = %b", sclk, cpol_0, cpol_1);
 
 endmodule
